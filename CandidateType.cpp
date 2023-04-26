678b115216e8ab1e808ab6aae331b5393853d28f
Rythmikk <109110648+Rythmikk@users.noreply.github.com>
2023-04-26 14:08:26 -0700

    Add files via upload
    here is what I have so far on candidate type class

---
+++ CandidateType.cpp
@@ -0,0 +1,31 @@
+#include "CandidateType.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+CandidateType::CandidateType()
+{
+    int numOfVotes = 0;
+    int campusVotes[NUM_OF_CAMPUSES] = {0};
+}
+int CandidateType::updateVotesByCampus()
+{
+
+}
+int CandidateType::getTotalVotes()
+{
+
+}
+int CandidateType::getVotesByCampus()
+{
+
+}
+void CandidateType::printCandidateInfo()
+{
+
+}
+CandidateType::~CandidateType()
+{
+
+}
