db51d3bb7f49fac166404f8d4ddf4a72a2afbd7d
Rythmikk <109110648+Rythmikk@users.noreply.github.com>
2023-04-25 21:31:27 -0700

    Update PersonType.cpp

--- PersonType.cpp
+++ PersonType.cpp
@@ -1,27 +1,62 @@
 #include "PersonType.h"
+#include <iostream>
+using namespace std;
 
-PersonType::PersonType() {
-	ssn = 0;
+PersonType::PersonType() //default constructor
+{
+    firstName;
+    lastName;
+    ssn = 0;
 }
-PersonType::PersonType(std::string fName, std::string lName, int SSN) {
-	ssn = SSN;
-	firstName = fName;
-	lastName = lName;
+PersonType::PersonType(string fName, string lName,int newSSN) //overloaded constructor
+{
+    firstName = fName;
+    lastName = lName;
+    ssn = newSSN;
 }
-void PersonType::printSSN() {
-	std::string s = std::to_string(ssn);
-	for (int i = 0; i < s.length(); i++) {
-		
-		if (i == 3 || i == 5) {
-			std::cout << "-";
-		}
-		std::cout << s[i];
-	}
+// these are all the public member function used to change private data and print object data
+void PersonType::setPersonInfo(string fName, string lName,int newSSN)
+{
+    firstName = fName;
+    lastName = lName;
+    ssn = newSSN;
 }
-PersonType::~PersonType() {
+string PersonType::getFirstName()
+{
+    return firstName;
+}
+string PersonType::getLastName()
+{
+    return lastName;
+}
+int PersonType::getSSN()
+{
+    return ssn;
+}
+void PersonType::printName()
+{
+    cout << lastName << ", " << firstName << endl;
+}
+void PersonType::printPersonInfo()
+{
+    printSSN();
+    cout << " " << firstName << " " << lastName;
+}
+void PersonType::printSSN()
+{
+    string SSN = to_string(ssn);
+    for (int i = 0; i < SSN.length(); i++)
+    {
+        if (i == 3 || i == 5)
+        {
+            cout << "-";
+        } 
+        cout << SSN[i];
+    }
 
 }
+//
+PersonType::~PersonType() //deconstructor
+{
 
-
-
-
+}
