0a8e212bda63dcb320c2f2024bab6edcdc6a06ac
L3M0Nnnn <shacobunny123@gmail.com>
2023-05-02 19:18:03 -0700

    driver fixing

--- Part B/Driver.cpp
+++ Part B/Driver.cpp
@@ -3,8 +3,11 @@
 using namespace std;
 
 int main() {
-	
+	string fName = "Bob", lName = "Cherries";
+	int ssn = 190456627;
 	CandidateType c1;
-	
+	c1.setPersonInfo(fName,lName,ssn);
+	c1.printCandidateInfo();
+
 
 }
