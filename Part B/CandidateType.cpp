57bcd5c4d83eed24505adef1558ca60425a32c1e
L3M0Nnnn <131841007+L3M0Nnnn@users.noreply.github.com>
2023-05-02 21:49:21 -0700

    Update CandidateType.cpp

--- Part B/CandidateType.cpp
+++ Part B/CandidateType.cpp
@@ -7,32 +7,32 @@ using namespace std;
 CandidateType::CandidateType()
 {
     int numOfVotes = 0;
-    int campusVotes[NUM_OF_CAMPUSES] = {0};
+    int campusVotes[NUM_OF_CAMPUSES] = { 0 };
 }
 void CandidateType::updateVotesByCampus(int numOfVotes, int choice)
 {
-    switch(choice)
+    switch (choice)
     {
-        case 1 :
-        if(choice == 1)
+    case 1:
+        if (choice == 1)
         {
             campusVotes[0] = numOfVotes;
         }
         break;
-        case 2 :
-        if(choice == 2)
+    case 2:
+        if (choice == 2)
         {
             campusVotes[1] = numOfVotes;
         }
         break;
-        case 3 :
-        if(choice == 3)
+    case 3:
+        if (choice == 3)
         {
             campusVotes[2] = numOfVotes;
         }
         break;
-        case 4 :
-        if(choice == 4)
+    case 4:
+        if (choice == 4)
         {
             campusVotes[3] = numOfVotes;
         }
@@ -43,7 +43,7 @@ int CandidateType::getTotalVotes() const
 {
     int totalVotes = 0;
 
-    for(int i = 0; i < NUM_OF_CAMPUSES; i++)
+    for (int i = 0; i < NUM_OF_CAMPUSES; i++)
     {
         totalVotes += campusVotes[i];
     }
@@ -51,11 +51,13 @@ int CandidateType::getTotalVotes() const
 }
 int CandidateType::getVotesByCampus(int choice) const
 {
-   return campusVotes[choice - 1];
+    return campusVotes[choice - 1];
 }
 void CandidateType::printCandidateInfo() const
 {
-    cout << PersonType::printSSN << " - " << PersonType::printName;
+    PersonType::printSSN();
+    cout << " - "; 
+    PersonType::printName();
 }
 CandidateType::~CandidateType()
 {
