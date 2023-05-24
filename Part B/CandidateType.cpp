fc7a426991f33d03fd27e13da8446c05c70d2826
L3M0Nnnn <shacobunny123@gmail.com>
2023-05-23 19:58:05 -0700

    correct to specifciations

--- Part B/CandidateType.cpp
+++ Part B/CandidateType.cpp
@@ -9,44 +9,42 @@ CandidateType::CandidateType()
     int numOfVotes = 0;
     int campusVotes[NUM_OF_CAMPUSES] = { 0 };
 }
-void CandidateType::updateVotesByCampus(int choice, int numOfVotes) //updates the total number of votes for a campus
+void CandidateType::updateVotesByCampus(int choice, int votes) //updates the total number of votes for a campus
 {
     switch (choice)
     {
     case 1:
         if (choice == 1)
         {
-            campusVotes[0] = numOfVotes;
+            campusVotes[0] = votes;
+            numOfVotes += votes;
         }
         break;
     case 2:
         if (choice == 2)
         {
-            campusVotes[1] = numOfVotes;
+            campusVotes[1] = votes;
+            numOfVotes += votes;
         }
         break;
     case 3:
         if (choice == 3)
         {
-            campusVotes[2] = numOfVotes;
+            campusVotes[2] = votes;
+            numOfVotes += votes;
         }
         break;
     case 4:
         if (choice == 4)
         {
-            campusVotes[3] = numOfVotes;
+            campusVotes[3] = votes;
+            numOfVotes += votes;
         }
         break;
     }
 }
 int CandidateType::getTotalVotes() const
 {
-    int tracking = 0;
-    for (int i = 0; i < NUM_OF_CAMPUSES; i++)
-    {
-        tracking += campusVotes[i];
-    }
-    numOfVotes = tracking;
     return numOfVotes;
 }
 int CandidateType::getVotesByCampus(int choice) const
--- Part B/CandidateType.h
+++ Part B/CandidateType.h
@@ -8,14 +8,14 @@ class CandidateType : public PersonType
 {
 public:
     CandidateType();
-    void updateVotesByCampus(int numOfVotes, int choice);
+    void updateVotesByCampus(int choice, int votes);
     int getTotalVotes() const;
     int getVotesByCampus(int choice) const;
     void printCandidateInfo() const;
     ~CandidateType();
 
 private:
-    int numOfVotes = 0;
+    int numOfVotes;
     int campusVotes[NUM_OF_CAMPUSES] = { 0 };
 };
 
